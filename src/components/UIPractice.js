61b33f9e48a13fa5f145fc46efb4a26aa33c434e
Naveen <raghuveer553@gmail.com>
2018-02-08 15:47:15 +0530

    Added select component as part of UI learning

--- src/components/UIPractice.js
+++ src/components/UIPractice.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
-var UIPractice = function(){
-  this.render = function(){
-    return (<div> Madala Naveen Raghuveer </div>);
+class UIPractice extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  render() {
+    return (<div> Madala Naveen Raghuveer
+      <br />
+      <select value={this.state.selectedday} onChange={this.handleChange}>
+        <option value="Thursday"> Thursday </option>
+        <option value="Friday"> Friday </option>
+        <option value="Saturday"> Saturday </option>
+      </select>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>);
+  }
+
+  handleChange(event) {
+    console.log("selected value is : " + event.target.value);
+    this.state.selectedday = event.target.value;
+    console.log("selected day according to state is : ",this.state.selectedday);
   }
 }
-
-UIPractice.prototype = new Component();
 
 export default UIPractice;
